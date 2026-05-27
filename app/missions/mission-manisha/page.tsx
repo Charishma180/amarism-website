645c75a2201f85e941fb23d41e50427c1bacd69b
Charishma <charishmapillapalem@gmail.com>
2026-05-27 14:44:52 +0530

    Updated missions banner mobile layout

--- app/missions/mission-manisha/page.tsx
+++ app/missions/mission-manisha/page.tsx
@@ -49,7 +49,7 @@ export default function MissionManishaPage() {
   <img
     src="/missions-banner.jpeg"
     alt="Our Missions"
-    className="w-full h-[260px] md:h-auto object-cover object-center"
+    className="w-full h-[260px] md:h-auto object-cover object-top"
   />
 </section>
       {/* PAGE 1 - INTRO */}
@@ -57,7 +57,7 @@ export default function MissionManishaPage() {
        id="internship-enrollment">
         <div className="grid lg:grid-cols-[38%_62%] min-h-[720px]">
           <div className="bg-gradient-to-b from-[#5c46ff] to-[#4338ca] text-white px-8 md:px-14 pt-6 pb-14 md:py-14 flex flex-col justify-center">
-            <span className="bg-white/15 px-5 py-3 rounded-xl text-xs font-bold w-fit mt-14">
+            <span className="bg-white/15 px-5 py-3 rounded-xl text-xs font-bold w-fit mb-8">
               FUTURE LEADERS
             </span>
 
